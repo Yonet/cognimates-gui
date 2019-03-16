a3a1f9429e10d765b099f31aaf99337cfca3dbe9
satvu <sarahvu@mit.edu>
2019-03-16 11:26:29 -0400

    Build for 97af66cf3820049b539a319522248811c09c6383

--- gui.js
+++ gui.js
@@ -149,7 +149,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
 
 var onClickLogo = function onClickLogo() {
-  window.location = 'https://scratch.mit.edu';
+  window.location = 'https://mitmedialab.github.io/cognimates-website/about/';
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
